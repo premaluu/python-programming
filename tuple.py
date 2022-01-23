3a3046024ddc5f22bdd95046c0e5d64d7ba1e573
premaluu <premalupadhyay56@gmail.com>
2022-01-23 18:34:42 +0530

    tuples in python

---
+++ tuple.py
@@ -0,0 +1,10 @@
+'''
+    name : tuple.py
+    Created by : Premal Upadhyay
+    Date : 23-01-2022
+'''
+
+numbers = (1, 2, 2, 3)
+#numbers[1] = 4 #Error
+print(numbers.count(2)) #returns the number of total occurance of given element
+print(numbers.index(2)) #returns the index of first occurance of given element
