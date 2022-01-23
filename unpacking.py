e2a7e2a68c2ff6f4b86cec50603da7a16a047ca4
premaluu <premalupadhyay56@gmail.com>
2022-01-23 18:37:27 +0530

    unpacking in python

---
+++ unpacking.py
@@ -0,0 +1,29 @@
+'''
+    name : unpacking.py
+    Created by : Premal Upadhyay
+    Date : 23-01-2022
+'''
+
+#unpacking list
+list = [2, 2, 4, 5, 6, 7]
+
+u, v, w, x, y, z = list
+
+print(u)
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
+
+#unpacking tuple
+tuple = (2, 2, 4, 5, 6, 7)
+
+u, v, w, x, y, z = tuple
+
+print(u)
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
