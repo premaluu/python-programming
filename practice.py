dbab2e586c329e8c921636714549175a0f4c2936
premalupadhyay_crest <premalupadhyay56@gmail.com>
2022-01-24 18:06:35 +0530

    tuple practice

--- practice.py
+++ practice.py
@@ -4,32 +4,6 @@
     Date : 23-01-2022
 '''
 
-list = [2, 2, 4, 6, 7, 8]
+tuple = (2, 3, 4, 5, 6)
 
-list.append(10)
-print(list)
-
-list.insert(2, 3)
-print(list)
-
-list.remove(3)
-print(list)
-
-list.clear()
-print(list)
-
-list = [2, 2, 4, 6, 7, 8]
-list.pop()
-print(list)
-
-print(list.count(2))
-
-list.sort()
-print(list)
-
-list.reverse()
-print(list)
-
-print(list.copy())
-
-print(list.index(2))
+print(tuple.index(4))
