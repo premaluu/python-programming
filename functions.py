c2c8a66daaeefdc15791845d31936e1ab3232ee5
premalupadhyay_crest <premalupadhyay56@gmail.com>
2022-01-26 00:08:19 +0530

    Function basics

---
+++ functions.py
@@ -0,0 +1,15 @@
+'''
+    name : functions.py
+    Created by : Premal Upadhyay
+    Date : 24-01-2022
+'''
+
+
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome aboard')
+
+
+print('Start')
+greet_user('John', 'Smith')
+print('Stop')
