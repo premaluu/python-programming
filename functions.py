614b3025430849b9d747129a6e730048b7fddfda
premalupadhyay_crest <premalupadhyay56@gmail.com>
2022-01-28 14:50:38 +0530

    Functions in python

--- functions.py
+++ functions.py
@@ -4,12 +4,17 @@
     Date : 24-01-2022
 '''
 
-
+#Function that greet the user
 def greet_user(first_name, last_name):
     print(f'Hi {first_name} {last_name}!')
     print('Welcome aboard')
 
+#Function that return square of number
+def square(number):
+    return number * number
 
 print('Start')
-greet_user('John', 'Smith')
+print('Greeting the user')
+greet_user(last_name='Smith', first_name='John')
+print(f'Square of 3 is : {square(3)}')
 print('Stop')
