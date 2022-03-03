2ac0f955b55b35101407a789a4d28433c4dafd3e
premalupadhyay_crest <premal.upadhyay@crestdatasys.com>
2022-03-03 11:26:12 +0530

    Docstring demo in python

---
+++ basics/docstring.py
@@ -0,0 +1,19 @@
+'''
+    name : docstring.py
+    Created by : Premal Upadhyay
+    Date : 03-03-2022
+'''
+
+
+def add(a, b):
+    """
+    This function is used to add two numbers
+
+    :param a: first operand for addition
+    :param b: second operand for addition
+    :return: returns addition of a and b
+    """
+    return a + b
+
+
+print(add(10, 15))
